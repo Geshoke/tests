d01f241f51357a337f96a8cd23dd9f8f4fff559a
Geshoke <accmpya@localhost.localdomain>
2020-01-21 13:56:23 +0300

    second comit after addition

---
+++ qgis2web_2020_01_21-13_55_57_657039/data/here_3.js
@@ -0,0 +1,1 @@
+var json_here_3 = {"type":"FeatureCollection","name":"here_3","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[36.964937483754767,-1.146264068427348]}}]}
